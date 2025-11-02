461c3c0708bca9bc4b3e6fcc493ffb9ef2b53454
Reed Terrell <terrell.systems@gmail.com>
2025-11-02 19:55:25 +0000

    improved api key logging, setup for prod docker container

--- internal/statistics/config/config.go
+++ internal/statistics/config/config.go
@@ -12,6 +12,12 @@ type APIKeysConfig struct {
 }
 
 func LoadConfig(path string) (*APIKeysConfig, error) {
+	// Intended to be a production config setup using an env variable
+	apiKeyFromEnv := os.Getenv("ALPHAVANTAGE_API_KEY")
+	if apiKeyFromEnv != "" {
+		return &APIKeysConfig{AlphaVantageAPIKey: apiKeyFromEnv}, nil
+	}
+	// If no env var use the toml config file, intended to be for development
 	if _, err := os.Stat(path); os.IsNotExist(err) {
 		return nil, fmt.Errorf("config file not found at path: %s", path)
 	}
@@ -22,7 +28,11 @@ func LoadConfig(path string) (*APIKeysConfig, error) {
 	}
 
 	if config.AlphaVantageAPIKey == "" {
-		return nil, fmt.Errorf("alphavantage API key is missing from config file")
+		errorMsg := "alphavantage API key is missing. \n"
+		errorMsg += "You can provide it either via the ALPHAVANTAGE_API_KEY environment variable (recommended for production),\n"
+		errorMsg += fmt.Sprintf("or by adding it to your config file at '%s' like this:\n\n", path)
+		errorMsg += `alphavantage = "YOUR_API_KEY_HERE"`
+		return nil, fmt.Errorf("%s", errorMsg)
 	}
 
 	return &config, nil
